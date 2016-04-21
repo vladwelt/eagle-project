bf7dd6ec9a73e095fd41d5e14090dfb5f01f2411
geovannaterceros <geovannaterceros@gmail.com>
2016-04-21 10:05:59 -0500

    The change in js/service.js, templates/tab-dash

--- www/js/services.js
+++ www/js/services.js
@@ -6,29 +6,24 @@ angular.module('starter.services', [])
   // Some fake testing data
   var chats = [{
     id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'img/ben.png'
+    name: 'Cristo',
+    lastText: 'Lugar turistico',
+    face: 'img/cristo.jpg'
   }, {
     id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'img/max.png'
+    name: 'Monumento',
+    lastText: 'Monumento a las Cholitas',
+    face: 'img/cholita.jpg'
   }, {
     id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'img/adam.jpg'
+    name: 'Avenida Heroinas',
+    lastText: 'Avenida Heroinas',
+    face: 'img/heroinas.jpg'
   }, {
     id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'img/perry.png'
-  }, {
-    id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'img/mike.png'
+    name: 'Parque Acuatico',
+    lastText: 'Parque Acuatico',
+    face: 'img/parque-acuatico2.jpg'
   }];
 
   return {
